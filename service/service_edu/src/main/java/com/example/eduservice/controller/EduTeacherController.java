12a54696b96e5bd2437d48018b831b8fb94f57be
StrongPosHao <lantian475587063@qq.com>
2020-09-07 00:38:55 +0800

    day 1 complete

--- service/service_edu/src/main/java/com/example/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/example/eduservice/controller/EduTeacherController.java
@@ -1,15 +1,22 @@
 package com.example.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.commonutils.R;
 import com.example.eduservice.entity.EduTeacher;
+import com.example.eduservice.entity.vo.TeacherQuery;
 import com.example.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -24,23 +31,115 @@ import java.util.List;
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
 
-    // 1 查询讲师表中所有数据
-    // rest风格
+    /**
+     * 查询讲师表中所有数据
+     */
     @Autowired
     private EduTeacherService teacherService;
 
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
-        return teacherService.list(null);
+    public R findAllTeacher() {
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items", list);
     }
 
-    // 2.逻辑删除讲师的方法
+    /**
+     * 逻辑删除讲师的方法
+     * @param id 讲师id
+     * @return
+     */
     @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
-        return teacherService.removeById(id);
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
+
+    /**
+     * 分页查询讲师的方法
+     * @param current 当前页
+     * @param limit 每页记录数
+     * @return
+     */
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit) {
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 调用方法实现分页
+        // 调用方法的时候，底层封装，把分页所有数据封装到pageTeacher对象里面
+        teacherService.page(pageTeacher, null);
+
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();  // 数据list集合
+
+//        Map map = new HashMap();
+//        map.put("total", total);
+//        map.put("rows", records);
+//        return R.ok().data(map);
+
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+
+        // 构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        // 判断条件值是否为空，如果不为空拼接条件
+        if (!StringUtils.isEmpty(name)) {
+            // 构建条件
+            wrapper.like("name", name);
+        }
+
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+//        wrapper
+        // 调用方法实现条件查询分页
+        teacherService.page(pageTeacher, wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();    // 数据list集合
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    /**
+     * 添加讲师接口的方法
+     */
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean save = teacherService.save(eduTeacher);
+        if (save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+
 
 }
 
